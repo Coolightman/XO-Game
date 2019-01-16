deeaad1766c8b5af055164a11c96e9071e20de6d
Coolightman <drugak48@gmail.com>
2019-01-16 13:26:41 +0300

    rebuild model Player end

--- src/com/coolightman/model/Player.java
+++ src/com/coolightman/model/Player.java
@@ -1,11 +1,23 @@
 package com.coolightman.model;
 
 public class Player {
-    private final String namePlayer;
+    private String namePlayer;
     private final String figurePlayer;
 
     public Player(String namePlayer, String figurePlayer) {
         this.namePlayer = namePlayer;
         this.figurePlayer = figurePlayer;
+    }
+
+    public String getNamePlayer() {
+        return namePlayer;
+    }
+
+    public String getFigurePlayer() {
+        return figurePlayer;
+    }
+
+    public void setNamePlayer(String namePlayer) {
+        this.namePlayer = namePlayer;
     }
 }
